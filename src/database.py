fe854c28840c31bfdfc4b471e9ec37db4cd01cb2
Björn Wedell <bjorn.wedell@actia.se>
2022-02-05 20:42:47 +0100

    Add database module skeleton

---
+++ src/database.py
@@ -0,0 +1,13 @@
+class Database:
+    def __init__(self):
+        pass
+
+    def CreateHunt(self, name, pwd):
+        return 1
+
+    def AddTreasure(self, hunt_id, pwd):
+        return 1
+
+    def FetchListOfTreasures(self, hunt_id, pwd):
+        return None
+
